743a7bdc732659848f8ab632e5875d6f2d8151f2
crugas <cschmidtuga@gmail.com>
2024-07-31 17:17:33 -0400

    Forgot to git commit previous... Adding logging to remove_missingtitles.py, more unittests for missingtitles_tests.py

--- python_scripts/remove_missingtitles.py
+++ python_scripts/remove_missingtitles.py
@@ -2,11 +2,18 @@
 # 'Missing Title' in the list title, removes the title and updates to ArchivesSpace
 
 from asnake.client.web_client import ASnakeAuthError
+from collections import namedtuple
+from loguru import logger
+from pathlib import Path
+import copy
+import os
 
 from secrets import *
 from asnake.client import ASnakeClient
 
-# TODO: add logging
+logger.remove()
+log_path = Path(f'../logs', 'remove-missingtitles_{time:YYYY-MM-DD}.log')
+logger.add(str(log_path), format="{time}-{level}: {message}")
 
 
 def client_login(as_api, as_un, as_pw):
@@ -31,19 +38,54 @@ def client_login(as_api, as_un, as_pw):
         return client
 
 
-def delete_missingtitle(object):
+def get_objects(object_metadata, test=""):
     """
-    Iterate through all the notes of a specific object (resource or archival object) and return object without 'Missing
-    Title' in notes list titles
+    Iterate through an object (resource or archival object) in a given repository and return new object without
+    'Missing Title' in notes lists
 
     Args:
-        object (dict): metadata for the specific object (resource or archival_object)
+        object_metadata (dict): resource or archival object metadata
+        test (str): if using unittests to record in log file, indicate with string input, default is empty string
 
     Returns:
-        new_object (dict): updated metadata for object without 'Missing Title' in notes list titles
+        new_obj (tuple): returns ERROR, PASS, or UPDATED, then the message or new object
     """
-    new_object = {}
-    for note in object['notes']:
+    Update = namedtuple('Update', 'Status Message')
+    new_object = copy.deepcopy(object_metadata)
+    if 'notes' in object_metadata:
+        if bool(object_metadata['notes']) is True:
+            new_obj_notes = delete_missingtitle(new_object['notes'])
+            if new_obj_notes:
+                logger.info(f'{test}Old object metadata: {object_metadata}')
+                new_object['notes'] = new_obj_notes
+                logger.info(f'{test}New object metadata: {new_object}')
+                new_obj = Update('UPDATED', new_object)
+                return new_obj
+            else:
+                new_obj = Update('PASS', f'No title in {object_metadata["title"]}')
+                return new_obj
+        else:
+            new_obj = Update('PASS', f'No notes with content in {object_metadata["title"]}')
+            return new_obj
+    else:
+        new_obj = Update('PASS', f'No notes in {object_metadata["title"]}')
+        return new_obj
+
+
+def delete_missingtitle(object_notes):
+    """
+    Iterate through all the notes of a specific object (resource or archival object) and return updated notes without
+    'Missing Title' in notes list titles
+
+    Args:
+        object_notes (list): metadata for the specific object's notes (resource or archival_object)
+
+    Returns:
+        new_notes (list): updated notes metadata for object without 'Missing Title' in notes list titles or empty list
+        if there are no notes found with 'Mising Title'
+    """
+    new_notes = []
+    for note in object_notes:
         if note['jsonmodel_type'] == 'note_multipart':
             if 'subnotes' in note:
                 for subnote in note['subnotes']:
@@ -52,48 +94,23 @@ def delete_missingtitle(object):
                             subnote['jsonmodel_type'] == 'note_chronology'):
                         if 'title' in subnote:
                             if subnote['title'] == 'Missing Title':
-                                print(object)
+                                new_notes = object_notes
                                 subnote.pop('title', None)
-                                new_object = object
-    return new_object
+    return new_notes
 
 
-def get_objects(client, repo, object_type):
-    """
-    Iterate through all objects (resources or archival objects) in a given repository and return new object without
-    'Missing Title' in notes lists
-
-    Args:
-        client (ASnake.client object): client object from ASnake.client to allow to connect to the ASpace API
-        repo (dict): metadata for a specific repository
-        object_type (str): specific type of object to iterate through (resources or archival_objects)
-
-    Returns:
-        new_resobj (dict): updated object record with "Missing Title" removed from notes lists
-    """
-    all_res_ids = client.get(f'{repo['uri']}/{object_type}', params={'all_ids': True}).json()
-    for res_id in all_res_ids:
-        object_md = client.get(f'{repo['uri']}/{object_type}/{res_id}').json()
-        if 'notes' in object_md:
-            if bool(object_md['notes']) is True:
-                new_resobj = delete_missingtitle(object_md)
-                if new_resobj:
-                    return new_resobj
-
-
-def update_object(client, new_object, repo, object_id):
+def update_object(client, original_objecturi, new_object):
     """
 
     Args:
         client (ASnake.client object): client object from ASnake.client to allow to connect to the ASpace API
+        original_objecturi (str): the original object's URI for posting to the client
         new_object (dict): the updated metadata for the object
-        repo (dict): metadata for a specific repository
-        object_id (str): identifier of the object in ArchivesSpace
 
     Returns:
         update_message (dict): ArchivesSpace response
     """
-    update_message = client.post(f'{repo['uri']}/resources/{object_id}', json=new_object).json()
+    update_message = client.post(f'{original_objecturi}', json=new_object).json()
     return update_message
 
 
@@ -102,13 +119,28 @@ def main():
     Runs the functions of the script, collecting, parsing, then updating metadata in for resources and archival objects
     with notes that have 'Missing Title' in their notes list titles
     """
+    object_types = ['resources', 'archival_objects']
     client = client_login(as_api, as_un, as_pw)
     all_repos = client.get('/repositories', params={'all_ids': True}).json()
     for repo in all_repos:
-        updated_resource = get_objects(client, repo, 'resources')
-        print(updated_resource)
-        updated_archobj = get_objects(client, repo, 'archival_objects')
-        print(updated_archobj)
+        for object_type in object_types:
+            all_res_ids = client.get(f'{repo['uri']}/{object_type}', params={'all_ids': True}).json()
+            for res_id in all_res_ids:
+                object_md = client.get(f'{repo['uri']}/{object_type}/{res_id}').json()
+                updated_object = get_objects(object_md)
+                if updated_object.Status == "ERROR":
+                    logger.error(updated_object.Message)
+                    print(updated_object.Status, updated_object.Message)
+                elif updated_object.Status == "PASS":
+                    pass
+                else:
+                    aspace_response = update_object(client, object_md['uri'], updated_object.Message)
+                    if 'error' in aspace_response:
+                        print(f'!!ERROR!!: {aspace_response}')
+                        logger.error(aspace_response)
+                    else:
+                        print(f'Updated object data: {aspace_response}')
+                        logger.info(f'{aspace_response}')
 
 
 if __name__ == "__main__":
---
+++ tests/missingtitles_tests.py
@@ -0,0 +1,74 @@
+# This script consists of unittests for update_znames.py
+import json
+import unittest
+
+from python_scripts.remove_missingtitles import *
+from secrets import *
+from test_data.missingtitles_testdata import *
+
+
+class TestClientLogin(unittest.TestCase):
+
+    def test_default_connection(self):
+        """Test using default connection info found in secrets.py"""
+        self.local_aspace = client_login(as_api, as_un, as_pw)
+        self.assertIsInstance(self.local_aspace, ASnakeClient)
+
+    def test_error_connection(self):
+        """Test using garbage input for ASnakeAuthError return"""
+        self.local_aspace = client_login("https://www.cnn.com", "garbage", "garbage")
+        self.assertEqual(self.local_aspace, ASnakeAuthError)
+
+
+class TestGetObjects(unittest.TestCase):
+
+    def test_missing_title(self):
+        """Tests that the returned users_data list is not empty"""
+        test_newobj = get_objects(test_object_metadata, test="UNITTEST")
+        for note in test_newobj.Message['notes']:
+            if 'subnotes' in note:
+                for subnote in note['subnotes']:
+                    if 'title' in subnote:
+                        self.fail(f'title found in {test_newobj.Message}')
+
+    def test_no_title(self):
+        """Tests if users_data is in JSON and 'username' key is in returned users_data"""
+        test_newobj = get_objects(test_object_metadata, test="UNITTEST")
+        test_updated_object = get_objects(test_newobj.Message, test="UNITTEST")
+        self.assertEqual(test_updated_object.Status, "PASS")
+        self.assertIn("No title in", test_updated_object.Message)
+
+    def test_no_note_content(self):
+        test_newobj = get_objects(test_object_metadata, test="UNITTEST")
+        test_newobj.Message['notes'] = []
+        test_updated_object = get_objects(test_newobj.Message, test="UNITTEST")
+        self.assertEqual(test_updated_object.Status, "PASS")
+        self.assertIn("No notes with content in", test_updated_object.Message)
+
+    def test_no_notes(self):
+        test_newobj = get_objects(test_object_metadata, test="UNITTEST")
+        test_newobj.Message.pop('notes', None)
+        test_updated_object = get_objects(test_newobj.Message, test="UNITTEST")
+        self.assertEqual(test_updated_object.Status, "PASS")
+        self.assertIn("No notes in", test_updated_object.Message)
+
+
+class TestDeleteMissingTitle(unittest.TestCase):
+    pass
+
+
+class TestUpdateObject(unittest.TestCase):
+
+    def test_aspace_post_response(self):
+        self.local_aspace = client_login(as_api, as_un, as_pw)
+        # self.assertEqual(test_response['status'], 'Updated')
+        # self.assertEqual(test_response['warnings'], [])
+
+    def test_bad_post(self):
+        self.local_aspace = client_login(as_api, as_un, as_pw)
+        # self.assertIn('error', test_response)
+        # self.assertEqual(test_response['error'], 'User not found')
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)

9f30710a9d128f497017b90b28b18f245bc3566d
crugas <SchmidtC@si.edu>
2024-10-15 14:06:45 -0400

    Added remove_nonalphanums() function and tests, logs successful ASnake client connection

--- python_scripts/update_resids.py
+++ python_scripts/update_resids.py
@@ -1,6 +1,7 @@
 # This script takes a CSV file of resource identifiers, edits them to standardize them to contain only alphanumeric
 # characters, except periods using those as separators, and posts those changes to ArchivesSpace
 import csv
+import json
 import re
 
 from asnake.client import ASnakeClient
@@ -10,8 +11,7 @@ from loguru import logger
 from pathlib import Path
 from secrets import *
 
-
-username_capture = re.compile(r'(?<=z-)(.*)(?=-expired)', re.UNICODE)
+alphanumeric_capture = re.compile(r'[a-zA-Z0-9.-]*', re.UNICODE)
 
 logger.remove()
 log_path = Path(f'../logs', 'update_resids_{time:YYYY-MM-DD}.log')
@@ -30,7 +30,6 @@ def client_login(as_api, as_un, as_pw):
     Returns:
         client (ASnake.client object): client object from ASnake.client to allow to connect to the ASpace API
     """
-    # aspace = ASpace(baseurl=as_api_stag, username=as_un, password=as_pw)
     client = ASnakeClient(baseurl=as_api, username=as_un, password=as_pw)
     try:
         client.authorize()
@@ -39,6 +38,7 @@ def client_login(as_api, as_un, as_pw):
         logger.error(f'Failed to authorize ASnake client. ERROR: {e}')
         return ASnakeAuthError
     else:
+        logger.info(f'Connected to ASnake client')
         return client
 
 def read_csv(all_ids_csv):
@@ -63,6 +63,56 @@ def read_csv(all_ids_csv):
         return resources
 
 
+def remove_nonalphanums(identifier_value):
+    """
+    Takes a string input for a resource identifier value, removes all non-alphanumeric characters except periods and
+    returns a new identifier value
+
+    Args:
+        identifier_value (str): the identifier value from id_0, id_1, id_2, and id_3 fields in a resource identifier
+
+    Returns:
+        updated_id_value (str): the identifier value from id_0, id_1, id_2, and id_3 fields in a resource identifier
+        containing only alphanumeric characters except periods
+    """
+    replaced_characters = re.findall(alphanumeric_capture, identifier_value)
+    no_alphanums = "".join(replaced_characters)
+    updated_id_value = no_alphanums.replace('-', '.')
+    return updated_id_value
+
+def concatenate_idfields(full_identifier):
+    """
+    Takes a list of the components for a resource identifier and concatenates them into a single string if their value
+    is not null
+
+    Args:
+        full_identifier (list): contains the values for id_0, id_1, id_2, and id_3 fields for resource identifiers
+
+    Returns:
+        concatenated_identifier (str): combined identifier from id_0, id_1, id_2, and id_3 fields for resource
+        identifiers
+    """
+    pass
+
+
+def check_eadid(updated_identifier, resource_uri, aspace_client):
+    """
+    Makes an ASpace API call to grab the resource JSON data, compares the updated identifier value to the EAD ID listed,
+    and if it matches, returns True. If it does not match, returns False.
+
+    Args:
+        updated_identifier (str): the concatenated identifier values from id_0, id_1, id_2, and id_3 fields in a
+        resource identifier containing only alphanumeric characters except periods
+        resource_uri (str): The URI for the resource record in ArchivesSpace
+        aspace_client (ASnake.client object): client object from ASnake.client to allow to connect to the ASpace API
+
+    Returns:
+        match_eadid (bool): if True, EAD ID and updated identifier match. If False, EAD ID and updated identifier do not
+        match
+    """
+    pass
+
+
 def update_resids(aspace_client, updated_resource):
     """
     Takes the updated resource JSON data and posts the new JSON data to ArchivesSpace
@@ -76,3 +126,21 @@ def update_resids(aspace_client, updated_resource):
 
     """
     pass
+
+
+def main():
+    aspace_client = client_login(as_api, as_un, as_pw)
+    resources = read_csv(f'../test_data/resource_accession_IDs_all.csv')
+    for resource in resources:
+        identifier_values = json.loads(resource['identifier'])
+        update_identifiers = []
+        for id_field in identifier_values:
+            if id_field:
+                update_identifiers.append(remove_nonalphanums(id_field))
+        print(update_identifiers)
+
+
+
+
+if __name__ == "__main__":
+    main()
--- tests/resids_tests.py
+++ tests/resids_tests.py
@@ -2,6 +2,7 @@
 import json
 import unittest
 
+from python_scripts.update_resids import remove_nonalphanums
 from python_scripts.update_znames import *
 from secrets import *
 # from test_data.resids_testdata import *
@@ -20,44 +21,55 @@ class TestClientLogin(unittest.TestCase):
         self.assertEqual(self.local_aspace, ASnakeAuthError)
 
 
-class TestGetUserData(unittest.TestCase):
+class TestRemoveAlphanums(unittest.TestCase):
 
-    def test_users_data(self):
-        """Tests that the returned users_data list is not empty"""
-        self.local_aspace = client_login(as_api, as_un, as_pw)
-        test_users = get_userdata(self.local_aspace)
-        self.assertFalse(not test_users)
-        self.assertIsInstance(test_users, list)
+    def test_no_change(self):
+        """Tests that the returned string is not changed by remove_nonalphanums()"""
+        no_changes = f'NMAI.AC.001.038'
+        updated_value = remove_nonalphanums(no_changes)
+        self.assertIsInstance(updated_value, str)
+        self.assertEqual(no_changes, updated_value)
 
-    def test_username_in_users_data(self):
-        """Tests if users_data is in JSON and 'username' key is in returned users_data"""
-        self.local_aspace = client_login(as_api, as_un, as_pw)
-        test_users = get_userdata(self.local_aspace)
-        for user in test_users:
-            test_json = json.dumps(user)
-            self.assertTrue(json.loads(test_json))  # This doesn't seem like the right way to do it...
-            self.assertTrue("username" in user)
+    def test_change_identifier(self):
+        """Tests that the returned string is changed by remove_nonalphanums()"""
+        change_value = f'''["NAA.1982-20:'''
+        updated_value = remove_nonalphanums(change_value)
+        self.assertIsInstance(updated_value, str)
+        self.assertEqual(updated_value, f'NAA.1982.20')
+
+    def test_non_ascii(self):
+        """Tests that remove_nonalphanums() fails gracefully when encountering non-ASCII characters"""
+        non_ascii_characters = {
+            'no_alphanums': f'''!@#$%^&*()_|\\?'/,~` ''',
+            'non_ascii': f'''é, à, ö, ñ, ü''',
+            'non_latin': f'''漢, こんにちは, به متنی''',
+            'symbols': f'''©, ®, €, £, µ, ¥''',
+            'emojis': f'''😀, 🌍, 🎉, 👋'''}
+        for incompatible_values in non_ascii_characters.values():
+            no_value = remove_nonalphanums(incompatible_values)
+            self.assertIsInstance(no_value, str)
+            self.assertEqual(no_value, f'')
 
 
-class TestParseZnames(unittest.TestCase):
-
-    def test_good_user(self):
-        good_user = {'username': 'z-simong-expired-'}
-        updated_username = parse_znames(good_user)
-        self.assertEqual(updated_username.Error, False)
-        self.assertEqual(updated_username.Message, 'simong')
-
-    def test_username_not_match(self):
-        nonmatch_user = {'username': 'simong'}
-        update_username = parse_znames(nonmatch_user)
-        self.assertEqual(update_username.Error, True)
-        self.assertIn('!!ERROR!!: Username does not match:', update_username.Message)
-
-    def test_username_not_found(self):
-        no_username = {'namenotuser': 'simong'}
-        update_username = parse_znames(no_username)
-        self.assertEqual(update_username.Error, True)
-        self.assertIn('!!ERROR!!: No username found:', update_username.Message)
+# class TestParseZnames(unittest.TestCase):
+#
+#     def test_good_user(self):
+#         good_user = {'username': 'z-simong-expired-'}
+#         updated_username = parse_znames(good_user)
+#         self.assertEqual(updated_username.Error, False)
+#         self.assertEqual(updated_username.Message, 'simong')
+#
+#     def test_username_not_match(self):
+#         nonmatch_user = {'username': 'simong'}
+#         update_username = parse_znames(nonmatch_user)
+#         self.assertEqual(update_username.Error, True)
+#         self.assertIn('!!ERROR!!: Username does not match:', update_username.Message)
+#
+#     def test_username_not_found(self):
+#         no_username = {'namenotuser': 'simong'}
+#         update_username = parse_znames(no_username)
+#         self.assertEqual(update_username.Error, True)
+#         self.assertIn('!!ERROR!!: No username found:', update_username.Message)
 
 
 class TestUpdateResIds(unittest.TestCase):

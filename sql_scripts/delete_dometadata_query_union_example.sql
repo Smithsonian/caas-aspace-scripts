7094b80889a82463a2598e093ce3d5fdcf528637
Mark <fordmadox@gmail.com>
2024-11-21 16:36:55 -0500

    modify the union example, and remove the original test file

--- sql_scripts/delete_dometadata_query_union_example.sql
+++ sql_scripts/delete_dometadata_query_union_example.sql
@@ -1,50 +1,49 @@
 SELECT 
-  COUNT(*) 
+  COUNT(*)
 FROM 
   (
     SELECT 
       do.id, 
-      do.repo_id 
+      do.repo_id
     FROM 
       digital_object AS do 
-      RIGHT JOIN lang_material ON lang_material.digital_object_id = do.id 
+      JOIN lang_material ON lang_material.digital_object_id = do.id 
+    UNION
+    SELECT 
+      do.id, 
+      do.repo_id
+    FROM 
+      digital_object AS do 
+      JOIN note ON note.digital_object_id = do.id 
+    UNION
+    SELECT 
+      do.id, 
+      do.repo_id
+    FROM 
+      digital_object AS do 
+      JOIN extent ON extent.digital_object_id = do.id 
+    UNION
+    SELECT 
+      do.id, 
+      do.repo_id
+    FROM 
+      digital_object AS do 
+      JOIN linked_agents_rlshp ON linked_agents_rlshp.digital_object_id = do.id 
+    UNION
+    SELECT 
+      do.id, 
+      do.repo_id
+    FROM 
+      digital_object AS do 
+      JOIN subject_rlshp ON subject_rlshp.digital_object_id = do.id 
     UNION 
     SELECT 
       do.id, 
-      do.repo_id 
+      do.repo_id
     FROM 
       digital_object AS do 
-      RIGHT JOIN note ON note.digital_object_id = do.id 
-    UNION 
-    SELECT 
-      do.id, 
-      do.repo_id 
-    FROM 
-      digital_object AS do 
-      RIGHT JOIN extent ON extent.digital_object_id = do.id 
-    UNION 
-    SELECT 
-      do.id, 
-      do.repo_id 
-    FROM 
-      digital_object AS do 
-      RIGHT JOIN linked_agents_rlshp ON linked_agents_rlshp.digital_object_id = do.id 
-    UNION 
-    SELECT 
-      do.id, 
-      do.repo_id 
-    FROM 
-      digital_object AS do 
-      RIGHT JOIN subject_rlshp ON subject_rlshp.digital_object_id = do.id 
-    UNION 
-    SELECT 
-      do.id, 
-      do.repo_id 
-    FROM 
-      digital_object AS do 
-      RIGHT JOIN date ON date.digital_object_id = do.id 
-    WHERE 
-      date.label_id != 2727
+   	JOIN date ON date.digital_object_id = do.id 
+		where date.label_id != 2727
   ) AS T 
 WHERE 
-  T.repo_id NOT IN (11, 23, 50, 52)
+T.repo_id NOT IN (11, 23, 50, 52)

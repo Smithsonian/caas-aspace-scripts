4caadd1486b32ab3dd8db1496e5faf25a07082f2
crugas <SchmidtC@si.edu>
2025-01-22 17:34:34 -0500

    Added updates from Lora's comments - update_locations.py system arguments and more requirements in requirements.txt (may need to pair this down?)

--- tests/updatelocations_tests.py
+++ tests/updatelocations_tests.py
@@ -4,15 +4,14 @@ import contextlib
 import io
 import unittest
 
+from dotenv import load_dotenv, find_dotenv
 from python_scripts.repeatable.update_locations import *
+from python_scripts.utilities import *
 
 # Hardcode to dev env
 env_file = find_dotenv(f'.env.dev')
 load_dotenv(env_file)
 local_aspace = ASpaceAPI(os.getenv('as_api'), os.getenv('as_un'), os.getenv('as_pw'))
-
-test_location = '/locations/1506 '
-test_no_repo_location = '/locations/55035'
 test_update_location = {'lock_version': 0, 'building': 'ACM', 'title': 'ACM, Stacks, Section 122 [Bay: 1, Column: A, Shelf: 2]', 'room': 'Stacks', 'area': 'Section 122', 'coordinate_1_label': 'Bay', 'coordinate_1_indicator': '1', 'coordinate_2_label': 'Column', 'coordinate_2_indicator': 'A', 'coordinate_3_label': 'Shelf', 'coordinate_3_indicator': '2', 'created_by': 'morrisj', 'last_modified_by': 'morrisj', 'create_time': '2025-01-07T19:09:37Z', 'system_mtime': '2025-01-07T19:09:37Z', 'user_mtime': '2025-01-07T19:09:37Z', 'jsonmodel_type': 'location', 'external_ids': [], 'functions': [], 'uri': '/locations/55035'}
 
 

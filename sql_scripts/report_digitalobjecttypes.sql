e3073cc7770824b1ff1f1f3f28723a82e1780262
crugas <SchmidtC@si.edu>
2025-03-17 11:42:41 -0400

    Update to fix 0 count, remove global repo, order by ascending repo name

--- sql_scripts/report_digitalobjecttypes.sql
+++ sql_scripts/report_digitalobjecttypes.sql
@@ -1,8 +1,9 @@
 -- This query counts all the digital objects with digital object types per each repository. 
 -- It also includes a count of all digital objects without a digital object type per repository.
+-- Now with proper 0s, due to count of digital object ids, rather than rows
 
 SELECT
-	repo.name as repository, ifnull(ev.value, "NO TYPE") as digital_object_type, COUNT(ifnull(ev.value, 0)) as count
+	repo.name as repository, coalesce(ev.value, "NO TYPE") as digital_object_type, COUNT(dobj.id) as count
 FROM
 	digital_object AS dobj
 LEFT JOIN
@@ -13,5 +14,9 @@ RIGHT JOIN
 	repository AS repo
     ON
     repo.id = dobj.repo_id
+WHERE
+	repo.name != 'Global repository'
 GROUP BY
 	ev.value, repo.name
+ORDER BY
+	repo.name ASC;

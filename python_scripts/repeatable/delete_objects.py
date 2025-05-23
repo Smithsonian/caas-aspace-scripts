ae0b5d070cdddb8c05f8cdf9b6b4f40bb1834a50
crugas <SchmidtC@si.edu>
2025-05-23 15:56:29 -0400

    Reformatted script to allow for global/local objects to be deleted, including unittests for retrieve_object_json()

--- python_scripts/repeatable/delete_objects.py
+++ python_scripts/repeatable/delete_objects.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-# This script takes a CSV of URIs and object type as inputs, grabs all the locations' JSON data using the API, saves
-# them to a jsonL file using the jsonl_path input, and then deletes them in ArchivesSpace. It's recommended to check to
-# see if locations have any top containers associated with them. You can run an SQL query to find any associated top
-# containers such as the following:
-# SELECT * FROM top_container_housed_at_rlshp
-# WHERE
-# location_id in (49324,49338,49323,etc.)
+# This script takes a CSV of URIs and object type as inputs, grabs all the objects' JSON data using the API, saves
+# them to a jsonL file using the jsonl_path input, and then deletes them in ArchivesSpace.
 import argparse
 import os
 import sys
@@ -14,11 +9,13 @@ from dotenv import load_dotenv, find_dotenv
 from loguru import logger
 from pathlib import Path
 
+from python_scripts.one_time_scripts.delete_dometadata import record_error
+
 sys.path.append(os.path.dirname('python_scripts'))  # Needed to import functions from utilities.py
 from python_scripts.utilities import ASpaceAPI, read_csv, write_to_file
 
 logger.remove()
-log_path = Path('../../logs', 'delete_locations_{time:YYYY-MM-DD}.log')
+log_path = Path('../../logs', 'delete_objects_{time:YYYY-MM-DD}.log')
 logger.add(str(log_path), format="{time}-{level}: {message}")
 
 # Find  and load environment-specific .env file
@@ -31,16 +28,39 @@ def parseArguments():
 
     parser.add_argument("csvPath", help="path to CSV input file", type=str)
     parser.add_argument("jsonPath", help="path to the JSONL file for storing data", type=str)
-    parser.add_argument("objectType", help="resources/archival_objects/digital_objects", type=str)
     parser.add_argument("-dR", "--dry-run", help="dry run?", action='store_true')
     parser.add_argument("--version", action="version", version='%(prog)s - Version 1.0')
 
     return parser.parse_args()
 
-
-def main(csv_path, jsonl_path, objectType, dry_run=False):
+def retrieve_object_json(object_uri, local_aspace):
     """
-    This script takes a CSV of URIs and object type as inputs, grabs all the locations' JSON data using the API, saves
+    Splits the given URI for the object via / and retrieves the object's JSON.
+    Args:
+        object_uri (DictReader object): the URI of the object
+        local_aspace (ASpaceAPI object): an instance of the ASpace API for connecting to the client
+
+    Returns:
+        object_json (dict): the JSON metadata for the object URI
+    """
+    try:
+        uri_parts = object_uri['uri'].split('/')
+        object_type, object_id = uri_parts[-2], uri_parts[-1]
+    except IndexError as spliterror:
+        record_error(f'retrieve_object_json() - Failed to split URI', spliterror)
+        return None
+    else:
+        if "repositories" in uri_parts:
+            repo_uri = f'repositories/{uri_parts[2]}'
+            object_json = local_aspace.get_object(object_type, object_id, repo_uri)
+        else:
+            object_json = local_aspace.get_object(object_type, object_id)
+        return object_json
+
+
+def main(csv_path, jsonl_path, dry_run=False):
+    """
+    This script takes a CSV of URIs and object type as inputs, grabs all the objects' JSON data using the API, saves
     them to a jsonL file using the jsonl_path input, and then deletes them in ArchivesSpace.
 
     The CSV should have the following data structure:
@@ -48,21 +68,19 @@ def main(csv_path, jsonl_path, objectType, dry_run=False):
     - Column 1 rows = /locations/#######
 
     Args:
-        csv_path (str): filepath of the CSV file with the location URIs
+        csv_path (str): filepath of the CSV file with the object URIs
         jsonl_path (str): filepath of the jsonL file for storing JSON data of objects before updates - backup
-        objectType (str): the type of object you want to delete. resources/archival_objects/digital_objects/locations
         dry_run (bool): if True, it prints the changed object_json but does not post the changes to ASpace
     """
     local_aspace = ASpaceAPI(os.getenv('as_api'), os.getenv('as_un'), os.getenv('as_pw'))
     for uri in read_csv(csv_path):
-        uri_parts = uri['uri'].split('/')
-        location = local_aspace.get_object(objectType, uri_parts[2])
-        write_to_file(jsonl_path, location)
-        if location:
+        object_json = retrieve_object_json(uri, local_aspace)
+        write_to_file(jsonl_path, object_json)
+        if object_json:
             if dry_run:
-                print(f'Deleted location {uri['uri']}')
+                print(f'Deleted object {uri['uri']}')
             else:
-                post_response = local_aspace.delete_object(location['uri'])
+                post_response = local_aspace.delete_object(object_json['uri'])
                 if post_response:
                     print(post_response)
 

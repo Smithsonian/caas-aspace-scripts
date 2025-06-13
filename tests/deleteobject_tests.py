b2de5c83d17a7b5e76f786d8498ad987c6eae444
Lora Woodford <lorawoodford@gmail.com>
2025-06-13 14:33:40 -0400

    Delete orphaned Digital Objects in CFCH repo #114

--- tests/deleteobject_tests.py
+++ tests/deleteobject_tests.py
@@ -1,6 +1,7 @@
 # This script consists of unittests for delete_object.py
 import contextlib
 import io
+import random
 import unittest
 
 from python_scripts.repeatable.delete_objects import *
@@ -11,9 +12,6 @@ from test_data.utilities_testdata import *
 env_file = find_dotenv('.env.dev')
 load_dotenv(env_file)
 local_aspace = client_login(os.getenv('as_api'), os.getenv('as_un'), os.getenv('as_pw'))
-test_dbconnection = ASpaceDatabase(os.getenv('db_un'), os.getenv('db_pw'), os.getenv('db_host'), os.getenv('db_name'),
-                                   int(os.getenv('db_port')))
-
 
 class TestArchivesSpaceClass(unittest.TestCase):
     good_aspace_connection = ASpaceAPI(os.getenv('as_api'), os.getenv('as_un'), os.getenv('as_pw'))
@@ -31,16 +29,17 @@ class TestArchivesSpaceClass(unittest.TestCase):
         """Tests that a local URI (one beginning with 'repositories/##/' is retrieved"""
         uri_parts = test_digital_object_dates['uri'].split('/')
         test_digobj = self.good_aspace_connection.get_object(uri_parts[-2], uri_parts[-1], f'{uri_parts[1]}/{uri_parts[2]}')
-        if 'error' not in test_digobj:
+        if test_digobj is not None:
             get_test_digobj = retrieve_object_json({'uri': test_digobj['uri']}, self.good_aspace_connection)
             self.assertIsNotNone(get_test_digobj)
             self.assertEqual(get_test_digobj['digital_object_id'], 'NMAI.AC.066.ref20')
         else:
+            test_digital_object_dates['digital_object_id'] = f'NMAI.AC.066.ref{random.randint(1, 100)}'
             test_digobj_new = self.good_aspace_connection.aspace_client.post('/repositories/12/digital_objects',
-                                                                             json=test_digital_object_dates)
+                                                                             json=test_digital_object_dates).json()
             get_test_digobj_new = retrieve_object_json({'uri': test_digobj_new['uri']}, self.good_aspace_connection)
             self.assertIsNotNone(get_test_digobj_new)
-            self.assertEqual(get_test_digobj_new['digital_object_id'], 'NMAI.AC.066.ref20')
+            self.assertTrue((get_test_digobj_new['digital_object_id']).startswith('NMAI.AC.066.ref'))
 
     def test_bad_uri(self):
         """Tests that a bad URI throws an error"""

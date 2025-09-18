ce0666a35adbcb623700d971c4165d54a6720233
crugas <SchmidtC@si.edu>
2025-09-18 16:28:45 -0400

    Linting updates

--- tests/deleteaaadigobjs_tests.py
+++ tests/deleteaaadigobjs_tests.py
@@ -1,15 +1,8 @@
 # This script consists of unittests for shared utilities.py
-import contextlib
-import io
-import json
-import os
 import unittest
 
-from dotenv import load_dotenv, find_dotenv
-from pathlib import Path
 from python_scripts.one_time_scripts.delete_aaadigobjs import *
 from python_scripts.utilities import *
-from test_data.utilities_testdata import *
 
 # Hardcode to dev env
 env_file = find_dotenv('.env.dev')
@@ -29,7 +22,7 @@ class TestDeleteAAADigObjs(unittest.TestCase):
         self.assertEqual(test_query, 'SELECT CONCAT("/repositories/30/digital_objects/", digobj.id) AS URI FROM `instance` AS inst JOIN instance_do_link_rlshp AS instrel ON instrel.instance_id = inst.id JOIN archival_object AS ao ON ao.id = inst.archival_object_id JOIN digital_object AS digobj ON digobj.id = instrel.digital_object_id WHERE ao.ref_id = "AAA.carninst_ref9955"')
 
 
-    def test_good_archobj_refid(self):
+    def test_bad_archobj_refid(self):
         """Test to see if a valid refID outputs the SQL query as expected."""
         bad_refid = {"AAA.carninst_ref9955"}
         with self.assertRaises(TypeError):

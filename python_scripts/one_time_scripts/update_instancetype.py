2f47c7225b28b1000a60f7ae0166448cc4a61d9c
crugas <62658840+crugas@users.noreply.github.com>
2026-02-18 15:43:08 -0500

    147-task-update-container-instance-types-for-nmah (#148)
    * Created update_instancetype.py in case we need it for bulk updates. Can move to repeatable directory if desired.
    
    * Remove unused function import
    
    * Fix for printing/logging correct original instance type
    
    * Added documentation and note saying this is hard-coded for NMAH

---
+++ python_scripts/one_time_scripts/update_instancetype.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python3
+# This script takes a CSV containing archival object IDs, retrieves the archival object JSON from the ArchivesSpace
+# API, checks for container instances associated with the archival object, updates the FIRST instance's instance type
+# to the updated_instance_value, then posts the result back to ArchivesSpace.
+# NOTE: This script is hard-coded to work with NMAH
+import argparse
+import os
+import sys
+
+from dotenv import load_dotenv, find_dotenv
+from loguru import logger
+from pathlib import Path
+
+sys.path.append(
+    os.path.dirname("python_scripts")
+)  # Needed to import functions from utilities.py
+from python_scripts.utilities import ASpaceAPI, read_csv, write_to_file
+
+# Find  and load environment-specific .env file
+env_file = find_dotenv(f'.env.{os.getenv("ENV", "dev")}')
+load_dotenv(env_file)
+
+
+def parseArguments():
+    """Parses the arguments fed to the script from the terminal or within a run configuration"""
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("csvPath", help="path to the CSV file for updating data", type=str)
+    parser.add_argument("jsonPath", help="path to the JSONL file for storing data", type=str)
+    parser.add_argument("logFolder", help="path to the log folder for storing log files", type=str)
+    parser.add_argument("-dR", "--dry-run", help="dry run?", action="store_true")
+    parser.add_argument("--version", action="version", version="%(prog)s - Version 1.0")
+
+    return parser.parse_args()
+
+
+def main(instances_csv, jsonl_path, dry_run=False):
+    """
+    NOTE: This script is hard-coded to work with NMAH in the repo_uri parameter when calling aspace_api.get_object()
+    Takes a CSV containing archival object IDs (column ao_ID), archival object ref IDs (column ao_refID),
+    and the value of the instance to update to (column updated_instance_value), retrieves the archival object JSON from
+    the ArchivesSpace API, checks for container instances associated with the archival object, updates the FIRST
+    instance's instance type to the updated_instance_value, then posts the result back to ArchivesSpace.
+
+
+    Args:
+        instances_csv (str): filepath of the CSV containing archival object URIs and instance type value to update
+        jsonl_path (str): filepath of the jsonL file for storing JSON data of objects before updates - backup
+        dry_run (bool): if True, it prints the changed object_json but does not post the changes to ASpace
+    """
+    aspace_api = ASpaceAPI(os.getenv("as_api"), os.getenv("as_un"), os.getenv("as_pw"))
+    archival_objects = read_csv(instances_csv)
+    for archival_object in archival_objects:
+        ao_json = aspace_api.get_object('archival_objects',
+                                    int(archival_object['ao_ID']),
+                                    '/repositories/20')
+        if ao_json:
+            write_to_file(jsonl_path, ao_json)
+            if 'instances' in ao_json:
+                for instance in ao_json['instances']:
+                    if 'digital_object' in instance:
+                        pass
+                    else:
+                        original_instance_type = instance['instance_type']
+                        instance['instance_type'] = archival_object['updated_instance_value']
+                        if dry_run:
+                            print(f'{archival_object['ao_refID']}: {original_instance_type} > '
+                                  f'{instance['instance_type']}')
+                            logger.info(f'{archival_object['ao_refID']}: {original_instance_type} > '
+                                        f'{instance['instance_type']}')
+                        else:  # TODO: This only updates the first instance - receive 'The record you tried to update has been modified since you fetched it.' when trying to update subsequent instances
+                            post_result = aspace_api.update_object(ao_json['uri'], ao_json)
+                            print(post_result)
+                            logger.info(post_result)
+
+
+# Call with `python update_instancetype.py <instances_csv_filepath>.csv <jsonl_filepath>.jsonl <log_folder_path>`
+if __name__ == "__main__":
+    args = parseArguments()
+
+    # Set up log file
+    logger.remove()
+    log_path = Path(args.logFolder, "update_instancetype_{time:YYYY-MM-DD}.log")
+    logger.add(str(log_path), format="{time}-{level}: {message}")
+
+    # Print arguments
+    logger.info(f"Running {sys.argv[0]} script with following arguments: ")
+    print(f"Running {sys.argv[0]} script with following arguments: ")
+    for arg in args.__dict__:
+        logger.info(str(arg) + ": " + str(args.__dict__[arg]))
+        print(str(arg) + ": " + str(args.__dict__[arg]))
+
+    # Run function
+    main(instances_csv=args.csvPath, jsonl_path=args.jsonPath, dry_run=args.dry_run)
